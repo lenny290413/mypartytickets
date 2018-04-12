59ac8241483a068df5dd8d2808cbd8d1b8f7511d
unknown <lenny290413@gmail.com>
2018-04-12 17:50:43 -0500

    menu al 90%

---
+++ includes/menu.php
@@ -0,0 +1,188 @@
+<header class="entero">
+   <div class="medio">
+       <a href="/" class="logo">
+            <img  src="imagenes/logo.png" alt="">   
+        </a>
+        <nav >
+            <ul class="menu">
+                <li class="items">Parks & Tickets
+                    <ul class="submenu">
+                        <span class="submenu_lado_izquierda">
+                            <div class="first_group group_submenu">
+                                <li class="tittle_submenu">Theme Parks</li>
+                                <a href="#" class="subitems disney_submenu">Disney
+
+                                    <div class="disney_encima sub_sub_menu_encima">
+                                        <h3 class="tittle_group_derecha">Disney</h3>
+                                        <div class="first_group_derecha">
+                                            <img src="imagenes/menu/disney.jpg" alt="">
+                                        </div>
+                                        <div class="secound_group_derecha">
+                                            <li class="tittle_group_derecha">Date</li>
+                                            <li><i class="icon-calendar"></i>From 20/04/2018 to 30/05/2019</li>
+                                        </div>
+                                        <div class="third_group_derecha">
+                                            <li class="tittle_group_derecha">Hora</li>
+                                            <li><i class="icon-clock"></i>9:00 AM  to 4:00 PM</li>
+                                        </div>
+                                    </div>
+
+                                </a>
+                                <a href="#" class="subitems universal_submenu">Universal Studio
+                                    
+                                <div class="universal_encima sub_sub_menu_encima">
+                                        <h3 class="tittle_group_derecha">Universal Studio</h3>
+                                        <div class="first_group_derecha">
+                                            <img src="imagenes/menu/universal.jpg" alt="">
+                                        </div>
+                                        <div class="secound_group_derecha">
+                                            <li class="tittle_group_derecha">Date</li>
+                                            <li><i class="icon-calendar"></i>From 20/04/2018 to 30/05/2019</li>
+                                        </div>
+                                        <div class="third_group_derecha">
+                                            <li class="tittle_group_derecha">Hora</li>
+                                            <li><i class="icon-clock"></i>9:00 AM  to 4:00 PM</li>
+                                        </div>
+                                    </div>
+
+                                </a>
+                                <a href="#" class="subitems seaworld_submenu">Seaworld
+                                    
+                                <div class="disney_encima sub_sub_menu_encima">
+                                        <h3 class="tittle_group_derecha">Seaworld</h3>
+                                        <div class="first_group_derecha">
+                                            <img src="imagenes/menu/seaworld.jpg" alt="">
+                                        </div>
+                                        <div class="secound_group_derecha">
+                                            <li class="tittle_group_derecha">Date</li>
+                                            <li><i class="icon-calendar"></i>From 20/04/2018 to 30/05/2019</li>
+                                        </div>
+                                        <div class="third_group_derecha">
+                                            <li class="tittle_group_derecha">Hora</li>
+                                            <li><i class="icon-clock"></i>9:00 AM  to 4:00 PM</li>
+                                        </div>
+                                    </div>
+
+                                </a>
+                                <a href="#" class="subitems bush_submenu">Bush Gardens
+                                    
+                                <div class="disney_encima sub_sub_menu_encima">
+                                        <h3 class="tittle_group_derecha">Bush Gardens</h3>
+                                        <div class="first_group_derecha">
+                                            <img src="imagenes/menu/bush.jpg" alt="">
+                                        </div>
+                                        <div class="secound_group_derecha">
+                                            <li class="tittle_group_derecha">Date</li>
+                                            <li><i class="icon-calendar"></i>From 20/04/2018 to 30/05/2019</li>
+                                        </div>
+                                        <div class="third_group_derecha">
+                                            <li class="tittle_group_derecha">Hora</li>
+                                            <li><i class="icon-clock"></i>9:00 AM  to 4:00 PM</li>
+                                        </div>
+                                    </div>
+
+                                </a>
+                                
+                            </div>
+                            <div class="secound_group group_submenu">
+                                <li class="tittle_submenu">Water Parks</li>
+                                <a href="#" class="subitems">Parque 1</a>
+                                <a href="#" class="subitems">Parque 2</a>
+                                <a href="#" class="subitems">Parque 3</a>
+                                <a href="#" class="subitems">Parque 4</a>
+                            </div>
+                            <div class="third_group group_submenu">
+                                <li class="tittle_submenu">Other attractions</li>
+                                <a href="#" class="subitems">attractions 1</a>
+                                <a href="#" class="subitems">attractions 2</a>
+                                <a href="#" class="subitems">attractions 3</a>
+                                <a href="#" class="subitems">attractions 4</a>
+                                <a href="#" class="subitems">attractions 5</a>
+                            </div>
+                        </span>
+                        <span class="submenu_lado_derecha">
+                            <div class="first_group_derecha">
+                                <img src="imagenes/menu/parks.jpg" alt="">
+                            </div>
+                            <div class="secound_group_derecha">
+                                <li class="tittle_group_derecha">Date</li>
+                                <li><i class="icon-calendar"></i>From 20/04/2018 to 30/05/2019</li>
+                            </div>
+                            <div class="third_group_derecha">
+                                <li class="tittle_group_derecha">Hora</li>
+                                <li><i class="icon-clock"></i>9:00 AM  to 4:00 PM</li>
+                            </div>
+                            
+                        </span>
+                    </ul>
+                </li>
+                <li class="items">Things to Do
+                <ul class="submenu">
+                        <span class="submenu_lado_izquierda">
+                            <div class="first_group group_submenu">
+                                <li class="tittle_submenu">Dinning</li>
+                                <a href="#" class="subitems ">Make Reservation</a>
+                                <a href="#" class="subitems ">All Dinning</a>
+                                <a href="#" class="subitems ">Dinning Plans</a>
+                            </div>
+                            <div class="secound_group group_submenu">
+                                <li class="tittle_submenu">Activities</li>
+                                <a href="#" class="subitems">attractions</a>
+                                <a href="#" class="subitems">Character Experiences</a>
+                                <a href="#" class="subitems">Entertainment</a>
+                                <a href="#" class="subitems">Events and Tours</a>
+                                <a href="#" class="subitems">Spas and Fitness Centers</a>
+                                <a href="#" class="subitems">Sports adn Recreation</a>
+                                <a href="#" class="subitems">Activity Photos</a>
+                            </div>
+                            <div class="third_group group_submenu">
+                                <li class="tittle_submenu">shopping</li>
+                                <a href="#" class="subitems">All Stores</a>
+                                <a href="#" class="subitems">Buy Merchandise</a>
+                            </div>
+                        </span>
+                        <span class="submenu_lado_derecha">
+                            <div class="first_group_derecha">
+                                <img src="imagenes/menu/seaworld.jpg" alt="">
+                            </div>
+                            <div class="secound_group_derecha">
+                                <li class="tittle_group_derecha">Date</li>
+                                <li><i class="icon-calendar"></i>From 20/04/2018 to 30/05/2019</li>
+                            </div>
+                            <div class="third_group_derecha">
+                                <li class="tittle_group_derecha">Hora</li>
+                                <li><i class="icon-clock"></i>9:00 AM  to 4:00 PM</li>
+                            </div>
+                            
+                        </span>
+                    </ul>
+                </li>
+                <li class="items">Help
+                <ul class="submenu submenu_help">
+                        <span class="submenu_lado_izquierda izquierda_help">
+                            <div class="first_group group_submenu">
+                                <a href="#" class="subitems">Help Center</a>
+                                <a href="#" class="subitems">Guest Services</a>
+                                <a href="#" class="subitems">Guest with Disabilities</a>
+                                <a href="#" class="subitems">Moms Panel</a>
+                                <a href="#" class="subitems">Contact Us</a>
+                                <img class="hada" src="imagenes/hada.png" alt="">
+                            </div>
+                        </span>
+                        <span class="submenu_lado_derecha">
+                            <div class="secound_group group_submenu">
+                                    <li class="tittle_submenu">Frequently Asked Questions</li>
+                                    <a href="#" class="subitems subitems_help">What items are not permited in the parks?</a>
+                                    <a href="#" class="subitems subitems_help">How does the doning plan work</a>
+                                    <a href="#" class="subitems subitems_help">Where can i find special offers?</a>
+                                    <a href="#" class="subitems subitems_help">What is Disney FastPass+ service?</a>
+                                    <li class="tittle_submenu view_all">View All</a>
+                            </div>
+                        </span>
+                    </ul>
+                </li>
+                <li class="items"><i class="icon-shopping-cart"></i>Cart</li>
+            </ul>
+        </div>
+   </div>
+</header>
